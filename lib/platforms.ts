768a53f74af4ef051857816b75e2325e6f54f26a
Shiwen Zhu <135702207+shiwenz59@users.noreply.github.com>
2026-08-03 10:34:25 -0400

    youtube music link added

--- lib/platforms.ts
+++ lib/platforms.ts
@@ -1,9 +1,10 @@
 export const musicPlatforms = [
   { num: '01', name: 'Apple Music', handle: 'Drifter Corp.',             href: 'https://music.apple.com/us/artist/drifter-corp/6782407914' },
   { num: '02', name: 'Spotify',     handle: 'Drifter Corp.',             href: 'https://open.spotify.com/artist/6WbDMwGpRtZdXUKEBrYhRR?si=-entC25UQVWl_1GNWj8XoA' },
-  { num: '03', name: 'Tidal',       handle: 'Drifter Corp.',             href: 'https://tidal.com/artist/81416212/u' },
-  { num: '04', name: 'NetEase Music', handle: '网易云音乐 · Drifter Corp.', href: 'https://163cn.tv/9ynREiF' },
-  { num: '05', name: 'QQ Music',    handle: 'QQ音乐 · Drifter Corp.',    href: 'https://c6.y.qq.com/base/fcgi-bin/u?__=P1csc1fl7Rw0' },
+  { num: '03', name: 'YouTube Music', handle: '@Drifter_Corp',          href: 'https://music.youtube.com/@Drifter_Corp' },
+  { num: '04', name: 'Tidal',       handle: 'Drifter Corp.',             href: 'https://tidal.com/artist/81416212/u' },
+  { num: '05', name: 'NetEase Music', handle: '网易云音乐 · Drifter Corp.', href: 'https://163cn.tv/9ynREiF' },
+  { num: '06', name: 'QQ Music',    handle: 'QQ音乐 · Drifter Corp.',    href: 'https://c6.y.qq.com/base/fcgi-bin/u?__=P1csc1fl7Rw0' },
 ]
 
 export const socialPlatforms = [
